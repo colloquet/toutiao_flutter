c1dd52afb1d3f7a4efadd7cb2fce913a378622a8
徐可乔 <xukeqiao@bytedance.com>
2020-11-20 18:54:17 +0800

    update

--- lib/feed_list.dart
+++ lib/feed_list.dart
@@ -18,8 +18,9 @@ class Feed extends StatelessWidget {
     final postTypes = [Post(), PostAlt(), PostAlt2()];
 
     return ListView.separated(
-      separatorBuilder: (BuildContext context, int index) =>
-          Container(height: 6.0, color: Color(0xfff2f2f2)),
+      separatorBuilder: (BuildContext context, int index) => story
+          ? Container(height: 6.0, color: Color(0xfff2f2f2))
+          : Divider(height: 1, indent: 16, endIndent: 16),
       itemCount: 100,
       itemBuilder: (BuildContext context, int index) {
         if (story && index == 0) {
