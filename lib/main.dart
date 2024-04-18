1649ba47033f7839fff5bb9ae2c59ac044b9f3f3
Colloque Tsui <colloquet@gmail.com>
2024-04-18 15:16:54 +0800

    remove web container

--- lib/main.dart
+++ lib/main.dart
@@ -161,187 +161,180 @@ class _MyHomePageState extends State<MyHomePage> {
         MediaQuery.of(context).platformBrightness == Brightness.dark
             ? Color(0xff1a1a1a)
             : Color(0xfff8f8f8);
-    return WebContainer(
-      app: DefaultTabController(
-        length: 10,
-        child: Container(
-          constraints: BoxConstraints(maxWidth: 500),
-          child: Scaffold(
-            backgroundColor: _selectedIndex == 3
-                ? profileBackgroundColor
-                : Theme.of(context).colorScheme.background,
-            appBar: _selectedIndex == 3
-                ? AppBar(
-                    systemOverlayStyle: SystemUiOverlayStyle(
-                        statusBarBrightness:
-                            MediaQuery.of(context).platformBrightness ==
-                                    Brightness.dark
-                                ? Brightness.dark
-                                : Brightness.light),
-                    leading: Icon(
-                      Icons.qr_code_outlined,
-                      color: Theme.of(context).textTheme.bodyLarge!.color,
+    return DefaultTabController(
+      length: 10,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 500),
+        child: Scaffold(
+          backgroundColor: _selectedIndex == 3
+              ? profileBackgroundColor
+              : Theme.of(context).colorScheme.background,
+          appBar: _selectedIndex == 3
+              ? AppBar(
+                  systemOverlayStyle: SystemUiOverlayStyle(
+                      statusBarBrightness:
+                          MediaQuery.of(context).platformBrightness ==
+                                  Brightness.dark
+                              ? Brightness.dark
+                              : Brightness.light),
+                  leading: Icon(
+                    Icons.qr_code_outlined,
+                    color: Theme.of(context).textTheme.bodyLarge!.color,
+                  ),
+                  actions: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Icon(
+                        Icons.mail_outline,
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                      ),
                     ),
-                    actions: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 16.0),
-                        child: Icon(
-                          Icons.mail_outline,
-                          color: Theme.of(context).textTheme.bodyLarge!.color,
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Icon(
+                        Icons.settings_outlined,
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                      ),
+                    ),
+                  ],
+                  backgroundColor: profileAppBarColor,
+                  elevation: 0,
+                )
+              : AppBar(
+                  elevation: 0,
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: MediaQuery.of(context).platformBrightness ==
+                                    Brightness.dark
+                                ? Color(0xff282828)
+                                : Colors.white,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.search,
+                                  color: textColor,
+                                  size: 20,
+                                ),
+                                SizedBox(width: 8),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      isDense: true,
+                                      hintText:
+                                          '苹果承认新机存问题｜小米 POCO M3苹果承认新机存问题｜小米 POCO M3',
+                                      hintStyle: TextStyle(color: textColor),
+                                      border: InputBorder.none,
+                                      contentPadding: EdgeInsets.all(0),
+                                    ),
+                                    // overflow: TextOverflow.fade,
+                                    style: TextStyle(
+                                      color: textColor,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 16,
+                                      height: 1,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 16.0),
-                        child: Icon(
-                          Icons.settings_outlined,
-                          color: Theme.of(context).textTheme.bodyLarge!.color,
-                        ),
+                      SizedBox(width: 8),
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.add_circle,
+                            color: iconColor,
+                            size: 28,
+                          ),
+                          Text('发布',
+                              style: TextStyle(fontSize: 10, color: iconColor)),
+                        ],
                       ),
                     ],
-                    backgroundColor: profileAppBarColor,
-                    elevation: 0,
-                  )
-                : AppBar(
-                    elevation: 0,
-                    title: Row(
+                  ),
+                  backgroundColor: MediaQuery.of(context).platformBrightness ==
+                          Brightness.dark
+                      ? Theme.of(context).colorScheme.background
+                      : Theme.of(context).colorScheme.secondary,
+                  bottom: PreferredSize(
+                    preferredSize: Size.fromHeight(40),
+                    child: Row(
                       children: [
                         Expanded(
                           child: Container(
                             decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
-                              color:
-                                  MediaQuery.of(context).platformBrightness ==
-                                          Brightness.dark
-                                      ? Color(0xff282828)
-                                      : Colors.white,
+                              color: Theme.of(context).colorScheme.background,
+                              border: Border(
+                                  bottom: BorderSide(color: borderColor)),
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.search,
-                                    color: textColor,
-                                    size: 20,
-                                  ),
-                                  SizedBox(width: 8),
-                                  Expanded(
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        isDense: true,
-                                        hintText:
-                                            '苹果承认新机存问题｜小米 POCO M3苹果承认新机存问题｜小米 POCO M3',
-                                        hintStyle: TextStyle(color: textColor),
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.all(0),
-                                      ),
-                                      // overflow: TextOverflow.fade,
-                                      style: TextStyle(
-                                        color: textColor,
-                                        fontWeight: FontWeight.normal,
-                                        fontSize: 16,
-                                        height: 1,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+                            height: 40,
+                            child: TabBar(
+                              unselectedLabelColor: textColor,
+                              labelColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              unselectedLabelStyle:
+                                  TextStyle(fontWeight: FontWeight.normal),
+                              labelStyle: TextStyle(
+                                  fontWeight: FontWeight.normal, fontSize: 14),
+                              labelPadding: EdgeInsets.symmetric(
+                                  vertical: 0, horizontal: 8),
+                              isScrollable: true,
+                              tabAlignment: TabAlignment.start,
+                              indicatorSize: TabBarIndicatorSize.label,
+                              indicatorColor: indicatorColor,
+                              tabs: [
+                                Tab(text: '关注'),
+                                Tab(text: '推荐'),
+                                Tab(text: '热榜'),
+                                Tab(text: '上海'),
+                                Tab(text: '国际'),
+                                Tab(text: '科技'),
+                                Tab(text: '娱乐'),
+                                Tab(text: '要闻'),
+                                Tab(text: '游戏'),
+                                Tab(text: '数码'),
+                              ],
                             ),
                           ),
-                        ),
-                        SizedBox(width: 8),
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.add_circle,
-                              color: iconColor,
-                              size: 28,
-                            ),
-                            Text('发布',
-                                style:
-                                    TextStyle(fontSize: 10, color: iconColor)),
-                          ],
                         ),
                       ],
                     ),
-                    backgroundColor:
-                        MediaQuery.of(context).platformBrightness ==
-                                Brightness.dark
-                            ? Theme.of(context).colorScheme.background
-                            : Theme.of(context).colorScheme.secondary,
-                    bottom: PreferredSize(
-                      preferredSize: Size.fromHeight(40),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).colorScheme.background,
-                                border: Border(
-                                    bottom: BorderSide(color: borderColor)),
-                              ),
-                              height: 40,
-                              child: TabBar(
-                                unselectedLabelColor: textColor,
-                                labelColor:
-                                    Theme.of(context).colorScheme.secondary,
-                                unselectedLabelStyle:
-                                    TextStyle(fontWeight: FontWeight.normal),
-                                labelStyle: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 14),
-                                labelPadding: EdgeInsets.symmetric(
-                                    vertical: 0, horizontal: 8),
-                                isScrollable: true,
-                                tabAlignment: TabAlignment.start,
-                                indicatorSize: TabBarIndicatorSize.label,
-                                indicatorColor: indicatorColor,
-                                tabs: [
-                                  Tab(text: '关注'),
-                                  Tab(text: '推荐'),
-                                  Tab(text: '热榜'),
-                                  Tab(text: '上海'),
-                                  Tab(text: '国际'),
-                                  Tab(text: '科技'),
-                                  Tab(text: '娱乐'),
-                                  Tab(text: '要闻'),
-                                  Tab(text: '游戏'),
-                                  Tab(text: '数码'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
                   ),
-            body: _widgetOptions.elementAt(_selectedIndex),
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              selectedFontSize: 12,
-              onTap: _onItemTapped,
-              currentIndex: _selectedIndex,
-              selectedItemColor: Color(0xfff04142),
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home_outlined),
-                  label: '头条',
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.play_arrow_outlined),
-                  label: '西瓜视频',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.ondemand_video_outlined),
-                  label: '放映厅',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person_outline),
-                  label: '我的',
-                ),
-              ],
-            ),
+          body: _widgetOptions.elementAt(_selectedIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 12,
+            onTap: _onItemTapped,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Color(0xfff04142),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: '头条',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.play_arrow_outlined),
+                label: '西瓜视频',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.ondemand_video_outlined),
+                label: '放映厅',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: '我的',
+              ),
+            ],
           ),
         ),
       ),
